b29299ac96e59aabf4126525c191209aca772a4f
Suyash458 <sne9x@outlook.com>
2015-10-15 20:26:28 +0530

    Changed config example

--- config-example.py
+++ config-example.py
@@ -7,3 +7,9 @@ test_url = 'https://api.telegram.org/<Your Bot Token>/getUpdates'
 glosbe_url = 'https://glosbe.com/gapi/translate?from=eng&dest=eng&format=json&phrase='
 
 thesaurus_api_key = 'XXXX XXXX'
+
+start_message = ''' Enter some message'''
+
+wordnik_url = 'http://api.wordnik.com:80/v4/word.json/'
+
+wordnik_api_key = 'your wordnik api key'
